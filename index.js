ed444ab8a5e2037b1808b35f993dbfdb1ce139de
Johnny Moya <profjmoya@gmail.com>
2021-10-18 20:03:34 -0500

    commit tree 039d947d1e2ab34975c73c9597d217a26bdf38af

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(arr, query ) {
+    const matchingNamesArray = arr.filter(name => name.toLowerCase() === query.toLowerCase());
+    
+    return matchingNamesArray;
+}
+
+function fuzzyMatch(arr, query ) {
+    const matchingNamesArray = arr.filter(name => name.toLowerCase().indexOf(query.toLowerCase()) == 0);
+
+    return matchingNamesArray;
+}
+
+function matchName(arr, query ) {
+    const matchingNamesArray = arr.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+
+    return matchingNamesArray;
+}
